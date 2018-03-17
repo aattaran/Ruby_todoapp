b48bbbee62e2d2f92f3adc59b3e265082af64c26
ali attaran <aliyarattaran@gmail.com>
2018-03-17 23:06:36 +0000

    make app production ready

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,14 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:edit, :update, :show, :destroy]
+  
+
 
   def new
     @todo = Todo.new
   end
   
   def create
-    @todo = Todo.new(todo_params)
+    
     if @todo.save
       flash[:notice] = 'todo was created'
       redirect_to todo_path(@todo)
@@ -15,16 +18,16 @@ class TodosController < ApplicationController
   end
   
   def show
-   @todo = Todo.find(params[:id])  
+  
   end
   
   def edit
-    @todo = Todo.find(params[:id])
+   
   end
   
   def update
     
-    @todo = Todo.find(params[:id])
+   
     if @todo.update(todo_params)
       flash[:notice] = "todo was updated"
       redirect_to todo_path(@todo)
@@ -33,10 +36,29 @@ class TodosController < ApplicationController
     end
   end
   
+  def index
+    
+    @todos = Todo.all
+    
+  end
+  
+  def destroy
+    
+    
+    @todo.destroy
+    flash[:notice] = 'todo was destroyed'
+    redirect_to todos_path
+    
+  end
   
   
   private
   
+    def set_todo
+      
+      @todo = Todo.find(params[:id])  
+    end
+    
     def todo_params
       params.require(:todo).permit(:name, :description)
     end
